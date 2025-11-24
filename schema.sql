bb802e3f5149dab01dbb8c2b6bfe9e628e455641
Kesha Bone <meadoremums@gmail.com>
2025-11-24 11:55:04 -0600

    created program table and inserted director data

--- schema.sql
+++ schema.sql
@@ -52,26 +52,26 @@ CONSTRAINT pk_actor PRIMARY KEY(actor_id)
 
 
  CREATE TABLE program (
-    movie_id MEDIUMINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    program_id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
     title VARCHAR (100) NOT NULL,
-    rating ENUM('G','PG', 'PG-13', 'R', 'NC-17', 'NR') DEFAULT 'NR',
+    personal_rating TINYINT UNSIGNED,
     runtime TIME, 
-    nationality CHAR (3),
+    program_rating ENUM('G','PG', 'PG-13', 'R', 'NC-17', 'NR', 'TV-G', 'TV-PG', 'TV-14', 'TV-MA') DEFAULT 'NR',
     yr_released YEAR,
-    budget INT UNSIGNED,
-    gross INT UNSIGNED,
-    production_id SMALLINT UNSIGNED,
-    showing ENUM('theater', 'direct-to-tv') DEFAULT 'theater',
-    poster BLOB,
+    format_id SMALLINT UNSIGNED,
+    producer_id SMALLINT UNSIGNED,
+    img_url BLOB,
+    description TEXT NOT NULL,
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-CONSTRAINT pk_movie PRIMARY KEY(movie_id),
-CONSTRAINT fk_production FOREIGN KEY(production_id) REFERENCES production (production_id)
+CONSTRAINT chk_personal_rating CHECK(personal_rating >= 1 AND personal_rating <= 10),
+CONSTRAINT pk_program PRIMARY KEY(program_id),
+CONSTRAINT fk_producer FOREIGN KEY(producer_id) REFERENCES producer (producer_id)
  );  
  
 
 CREATE TABLE program_to_streaming (
-program_id MEDIUMINT UNSIGNED NOT NULL,
+program_id BIGINT UNSIGNED NOT NULL,
 streaming_platform_id TINYINT UNSIGNED NOT NULL,
 CONSTRAINT fk_prog_str FOREIGN KEY (program_id) REFERENCES program (program_id),
 CONSTRAINT fk_str_prog FOREIGN KEY(streaming_platform_id) REFERENCES streaming_platform (streaming_platform_id)
@@ -79,30 +79,27 @@ CONSTRAINT fk_str_prog FOREIGN KEY(streaming_platform_id) REFERENCES streaming_p
 
 
 CREATE TABLE program_to_actor (
-    program_id MEDIUMINT UNSIGNED NOT NULL,
+    program_id BIGINT UNSIGNED NOT NULL,
     actor_id MEDIUMINT UNSIGNED NOT NULL,
    CONSTRAINT fk_prog_act FOREIGN KEY (program_id) REFERENCES program (program_id),
 CONSTRAINT fk_act_prog FOREIGN KEY(actor_id) REFERENCES actor (actor_id)
 );
 
-
-CREATE TABLE movie_to_genre (
-movie_id MEDIUMINT UNSIGNED NOT NULL,
-genre_id TINYINT UNSIGNED NOT NULL,
-CONSTRAINT fk_mov_gen FOREIGN KEY (movie_id) REFERENCES movie (movie_id),
-CONSTRAINT fk_gen_mov FOREIGN KEY(genre_id) REFERENCES genre (genre_id)
-);
-
-CREATE TABLE movie_to_director (
-movie_id MEDIUMINT UNSIGNED NOT NULL,
+CREATE TABLE program_to_director (
+program_id BIGINT UNSIGNED NOT NULL,
 director_id SMALLINT UNSIGNED NOT NULL,
-CONSTRAINT fk_mov_dir FOREIGN KEY (movie_id) REFERENCES movie (movie_id),
-CONSTRAINT fk_dir_mov FOREIGN KEY(director_id) REFERENCES director (director_id)
+CONSTRAINT fk_prog_dir FOREIGN KEY (program_id) REFERENCES program (program_id),
+CONSTRAINT fk_dir_prog FOREIGN KEY(director_id) REFERENCES director (director_id)
 );
 
 
 
-
+CREATE TABLE program_to_producer (
+program_id BIGINT UNSIGNED NOT NULL,
+producer_id SMALLINT UNSIGNED NOT NULL,
+CONSTRAINT fk_prog_producer FOREIGN KEY (program_id) REFERENCES program (program_id),
+CONSTRAINT fk_producer_prog FOREIGN KEY(producer_id) REFERENCES producer (producer_id)
+);
 
 
 -- ALTERATIONS
